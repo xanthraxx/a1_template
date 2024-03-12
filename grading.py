e20773b1c1f3775379efa6dff703685b6ff3133f
chenyuanz <chenyuanz@student.unimelb.edu.au>
2024-03-12 20:26:18 +1100

    grading update

--- grading.py
+++ grading.py
@@ -88,15 +88,15 @@ class Grades:
             if self.points[q] >= self.maxes[q]:
                 completedQuestions.add(q)
 
-            print('\n### Question %s: %d/%d ###\n' % (q, self.points[q], self.maxes[q]))
+            print('\n### Question %s: %.02f/%d ###\n' % (q, self.points[q], self.maxes[q]))
 
         print('\nFinished at %d:%02d:%02d' % time.localtime()[3:6])
         print("\nProvisional grades\n==================")
 
         for q in self.questions:
-            print('Question %s: %d/%d' % (q, self.points[q], self.maxes[q]))
+            print('Question %s: %.02f/%d' % (q, self.points[q], self.maxes[q]))
         print('------------------')
-        print('Total passed test cases: %d/%d' % (self.points.totalCount(), sum(self.maxes.values())))
+        print('Total passed test cases: %.02f/%d' % (self.points.totalCount(), sum(self.maxes.values())))
         if bonusPic and self.points.totalCount() == 25:
             print("""
 
